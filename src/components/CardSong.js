53425b5c848752a4dcf643a68284559400c339c2
VictorAnizar <victoranizarm@live.com>
2021-10-17 13:40:30 -0500

    creando tarjetas

---
+++ src/components/CardSong.js
@@ -0,0 +1,67 @@
+import * as React from 'react';
+// import { styled } from '@mui/material/styles';
+import Card from '@mui/material/Card';
+import CardHeader from '@mui/material/CardHeader';
+import CardMedia from '@mui/material/CardMedia';
+import CardContent from '@mui/material/CardContent';
+import CardActions from '@mui/material/CardActions';
+// import Collapse from '@mui/material/Collapse';
+import Avatar from '@mui/material/Avatar';
+import IconButton from '@mui/material/IconButton';
+import Typography from '@mui/material/Typography';
+import { red } from '@mui/material/colors';
+import FavoriteIcon from '@mui/icons-material/Favorite';
+import ShareIcon from '@mui/icons-material/Share';
+// import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
+import MoreVertIcon from '@mui/icons-material/MoreVert';
+
+
+class CardSong extends React.Component {
+  
+
+   render(){
+    return (
+        <Card sx={{ maxWidth: 345, borderRadius:3 }}>
+            <CardHeader
+                avatar={
+                    <Avatar sx={{ bgcolor: red[500] }} aria-label="recipe">
+                        R
+                    </Avatar>
+                }
+                action={
+                    <IconButton aria-label="settings">
+                        <MoreVertIcon />
+                    </IconButton>
+                }
+                title={this.props.song.title}
+                subheader="September 14, 2016"
+            />
+            <CardMedia
+                component="img"
+                height="194"
+                image="https://mui.com/static/images/cards/paella.jpg"
+                alt="Paella dish"
+            />
+            <CardContent>
+                <Typography variant="body2" color="text.secondary">
+                    Artist: {this.props.song.info.artist}
+                    <br/>
+                    Gender: {this.props.song.info.gender}
+                </Typography>
+            </CardContent>
+            <CardActions disableSpacing>
+                <IconButton aria-label="add to favorites">
+                    <FavoriteIcon />
+                </IconButton>
+                <IconButton aria-label="share">
+                    <ShareIcon />
+                </IconButton>
+               
+            </CardActions>
+           
+        </Card>
+    );
+   }
+}
+
+export default CardSong;
