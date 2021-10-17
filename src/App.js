9d25fdf271c9d168e4c9da7ac4774d810175d1cd
VictorAnizar <victoranizarm@live.com>
2021-10-17 11:45:30 -0500

    jalando cambios de rama leo a victor

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ class App extends React.Component {
   render() {
     return (
       <div className="layout">
-        <UserGreeting/> 
+        {/* <UserGreeting/>  */}
         <Header />
         <ContentMain/>
         <MusicList/>
