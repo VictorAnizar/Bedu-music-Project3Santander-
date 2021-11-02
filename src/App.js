ebe9adaa5690dc75a5f2cbe259d1be7ce837541c
VictorAnizar <victoranizarm@live.com>
2021-11-02 17:54:34 -0600

    terminando de poner ruteo

--- src/App.js
+++ src/App.js
@@ -4,17 +4,84 @@ import Header from './components/globals/Header';
 import UserGreeting from './components/globals/UserGreeting';
 import ContentMain from './components/ContentMain';
 import MusicList from './components/MusicList';
+import Contacto from './components/Contacto';
+import TuMusica from './components/TuMusica';
+import Cuenta from './components/Cuenta';
+import Descubre from './components/Descubre';
 
-const App =()=> {
-    return (
-      <div className="layout">
+
+//imports para el ruteo
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+
+import { LinkContainer } from 'react-router-bootstrap';
+
+const App = () => {
+  return (
+    <div className="layout">
+
+      <Router>
         <Header />
-        {/* <UserGreeting/>  */}
-        <ContentMain/>
-        <MusicList/>
-        <Footer />
-      </div>
-    )
+
+
+        <Switch>
+          <Route exact path="/" render={() => {
+            return (
+              <>
+                <ContentMain />
+                <MusicList />
+              </>
+            )
+
+          }}>
+          </Route>
+          <Route exact path="/tu-musica" render={() => {
+            return (
+              <>
+                <TuMusica />
+              </>
+            )
+
+          }}>
+          </Route>
+          <Route exact path="/descubre"
+            render={() => {
+
+              return (
+                <>
+                  <Descubre />
+                </>
+              )
+            }}>
+          </Route>
+          <Route exact path="/contacto" render={() => {
+
+            return (
+              <>
+                <Contacto />
+              </>
+            )
+          }}>
+
+          </Route>
+          <Route exact path="/cuenta" render={() => {
+
+            return (
+              <>
+                <Cuenta />
+              </>
+            )
+          }}>
+          </Route>
+        </Switch>
+      </Router>
+    </div>
+
+  )
 }
 
 export default App;
