40ebfa6919730a50aa370bb7b703dc27b925c785
Leoguzs <lguzs96@gmail.com>
2021-11-02 18:25:46 -0600

    cambios en estilos

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
-import React from 'react'
-import Footer from './components/globals/Footer'
-import Header from './components/globals/Header';
-import UserGreeting from './components/globals/UserGreeting';
-import ContentMain from './components/ContentMain';
-import MusicList from './components/MusicList';
+import React from "react";
+import Footer from "./components/globals/Footer";
+import Header from "./components/globals/Header";
+import UserGreeting from "./components/globals/UserGreeting";
+import ContentMain from "./components/ContentMain";
+import MusicList from "./components/MusicList";
 
 class App extends React.Component {
   render() {
     return (
       <div className="layout">
         <Header />
-        <UserGreeting/> 
-        <ContentMain/>
-<<<<<<< HEAD
-        <MusicList/>
-=======
-        <MusicList/> 
->>>>>>> main
+        <UserGreeting />
+        <ContentMain />
+        <MusicList />
         <Footer />
       </div>
-    )
+    );
   }
 }
 
